66b5b7ebed600a2795f2aceac8b41019e8423c67
David Snopek <dsnopek@gmail.com>
2017-02-15 16:03:53 -0600

    Of course, that didn't work!

--- scripts/check-overridden.sh
+++ scripts/check-overridden.sh
@@ -10,7 +10,14 @@ PANOPOLY_FEATURES="panopoly_core panopoly_demo panopoly_images panopoly_pages pa
 $DRUSH $DRUSH_ARGS en -y diff features
 
 OVERRIDDEN=0
+FIRST=1
 for panopoly_feature in $PANOPOLY_FEATURES; do
+  if [ x$FIRST = x1 ]; then
+	# Run the first features-diff twice.
+    $DRUSH $DRUSH_ARGS features-diff $panopoly_feature 2>&1 >/dev/null
+	FIRST=0
+  fi
+
   echo "Checking $panopoly_feature..."
   if $DRUSH $DRUSH_ARGS features-diff $panopoly_feature 2>&1 | grep -v 'Active config matches stored config'; then
     OVERRIDDEN=1
